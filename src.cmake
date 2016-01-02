bcca6a1a29817d4a760242ac49da7efab92a1182
dolcetriade <vcelestialragev@gmail.com>
2016-01-02 00:38:56 -0800

    Add unv.buildables API

--- src.cmake
+++ src.cmake
@@ -87,6 +87,8 @@ set(GAMESHAREDLIST
 
     ${GAMELOGIC_DIR}/shared/lua/Weapons.h
     ${GAMELOGIC_DIR}/shared/lua/Weapons.cpp
+    ${GAMELOGIC_DIR}/shared/lua/Buildables.h
+    ${GAMELOGIC_DIR}/shared/lua/Buildables.cpp
 )
 
 set(CGAMELIST

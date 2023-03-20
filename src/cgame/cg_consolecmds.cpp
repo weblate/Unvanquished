24969beaa54d9a09b9027105b0a47a7bb749dac5
Morel Bérenger <berengermorel76@gmail.com>
2023-03-20 12:04:15 +0100

    cgame: use std::lower_bound a bit more often

--- src/cgame/cg_consolecmds.cpp
+++ src/cgame/cg_consolecmds.cpp
@@ -431,11 +431,29 @@ static void CG_BeaconMenu_f()
 	Rocket_DocumentAction( rocketInfo.menu[ ROCKETMENU_BEACONS ].id, "show" );
 }
 
-static const struct
+static const struct cg_cmd_t
 {
 	const char *cmd;
 	void ( *function )();
 	void ( *completer )();
+
+	bool operator<( cg_cmd_t const& other ) const
+	{
+		if ( other.cmd )
+		{
+			return Q_stricmp( cmd, other.cmd ) < 0;
+		}
+		return true;
+	}
+
+	bool operator<( char const* other ) const
+	{
+		if ( other )
+		{
+			return Q_stricmp( cmd, other ) < 0;
+		}
+		return true;
+	}
 } commands[] =
 {
 	{ "+scores",           CG_ShowScores_f,        0                },
@@ -536,13 +554,9 @@ Cmd_Argc() / Cmd_Argv()
 bool ConsoleCommand()
 {
 	char buffer[BIG_INFO_STRING];
-	consoleCommand_t *cmd;
 
-	cmd = (consoleCommand_t*) bsearch( CG_Argv( 0 ), commands,
-	               ARRAY_LEN( commands ), sizeof( commands[ 0 ] ),
-	               cmdcmp );
-
-	if ( !cmd || !cmd->function )
+	const cg_cmd_t *cmd = std::lower_bound( std::begin( commands ), std::end( commands ), CG_Argv( 0 ) );
+	if ( cmd == std::end( commands ) || !cmd->function )
 	{
 		//This command was added to provide completion of server-side commands
 		//forward it to the server
@@ -554,6 +568,7 @@ bool ConsoleCommand()
 	{
 		cmd->function();
 	}
+
 	return true;
 }
 
@@ -603,8 +618,6 @@ Cmd_Argc() / Cmd_Argv()
 void CompleteCommand( int argNum )
 {
 	const char *cmd;
-	unsigned i;
-
 	Q_UNUSED(argNum);
 
 	cmd = CG_Argv( 0 );
@@ -614,12 +627,9 @@ void CompleteCommand( int argNum )
 		cmd++;
 	}
 
-	for ( i = 0; i < ARRAY_LEN( commands ); i++ )
+	const cg_cmd_t *it = std::lower_bound( std::begin( commands ), std::end( commands ), CG_Argv( 0 ) );
+	if ( it != std::end( commands ) )
 	{
-		if ( !Q_stricmp( cmd, commands[ i ].cmd ) && commands[ i ].completer )
-		{
-			commands[ i ].completer();
-			return;
-		}
+		it->completer();
 	}
 }
